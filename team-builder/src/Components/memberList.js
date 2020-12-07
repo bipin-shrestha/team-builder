1acc1032db59039f19bb8c49d383e7848cf38ac2
bipin-shrestha <iambpn.shrestha@gmail.com>
2020-12-06 17:11:07 -0800

    completed the Builder Assignment

---
+++ team-builder/src/Components/memberList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Member = props => {
+    return (
+        <div>
+            {props.members.map(member => (
+                <div>
+                    <h2>{member.name}</h2>
+                    <p>{member.email}</p>
+                    <p>{member.role}</p>
+                </div>
+            ))}
+        </div>
+    );
+ };
+
+export default Member;
---
+++ team-builder/src/members-data.js
@@ -0,0 +1,25 @@
+const data = [
+    {   
+        name: "Bipin Shrestha",
+        email: "bipin.shrestha@gmail.com",
+        role: "Developer"
+    },
+    {
+        name: "Anu Shrestha",
+        email: "shrestha.anu@gmail.com",
+        role: "Developer"
+    },
+    {
+        name: "Brizen Rai",
+        email: "brizen.rai@gmail.com",
+        role: "Developer"
+    },
+    {
+        name: "Samden Lama",
+        email: "lama.samden@gmail.com",
+        role: "Developer"
+    }
+
+];
+
+export default data;
